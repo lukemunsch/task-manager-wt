7927f005d140655050ceb9d99e81593980579578
lukemunsch <lpmunsch@gmail.com>
2022-01-05 23:00:47 +0000

    add content changes to multiple files to fix bugs

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,7 +2,7 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    # create table scheme
+    # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
@@ -13,18 +13,16 @@ class Category(db.Model):
 
 
 class Task(db.Model):
-    #  create Task table scheme
+    # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
-    is_urgent = db.Column(db.Boolean, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return f"#{self.id} - Task: {self.task_name} | Urgent: {self.is_urgent}"
-        # return "#{0} - Task: {1} | Urgent: {2}".format(
-        #     self.id, self.task_name, self.is_urgent
-        # )                                           is another way this can be written
-
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
