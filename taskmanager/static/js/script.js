03edcb0b3f2e402d54c7adfe1dbb9208d737fabf
lukemunsch <lpmunsch@gmail.com>
2022-01-06 23:41:18 +0000

    add new add tasks form and content

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // date picker code
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "DD MMMM, YYYY",
+    i18n: {
+      done: "Select"
+    }
   });
+
+  // select list input
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
+});
